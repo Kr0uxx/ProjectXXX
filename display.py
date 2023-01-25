6c35d8ab3d969d62abbd03f99bd21df1c18df91d
max <m7a7x7gg@gmail.com>
2023-01-25 20:53:05 +0300

    Revert "menu and health bar have been added and also bg"
    This reverts commit 375ae7de

--- display.py
+++ display.py
@@ -1,28 +1,3 @@
-import pygame
-
-
 class Display:
-    def __init__(self, screen, width, height, hp_percent, mana_percent):
-        self.hp_percent = hp_percent
-        self.mana_percent = mana_percent
-        self.width_hp, self.height_hp = 250 * self.hp_percent * 0.01, 30
-        self.width_mana, self.height_mana = 250 * self.mana_percent * 0.01, 30
-        self.width = width
-        self.screen = screen
-        self.bar_img = pygame.image.load("graphics\\display\\bar.png")
-        self.bar_img = pygame.transform.scale(self.bar_img, (300, 100))
-
-    def hp_subtraction(self, damage):
-        self.hp_percent -= damage * 0.001
-        self.width_hp = self.width * 0.168 * self.hp_percent
-
-    def run(self):
-        pygame.draw.rect(self.screen, (51, 51, 51),
-                         (23, 10, 250, self.height_hp))
-        pygame.draw.rect(self.screen, (51, 51, 51),
-                         (23, 68, 250, self.height_mana))
-        pygame.draw.rect(self.screen, (176, 0, 33),
-                         (23, 10, self.width_hp, self.height_hp))
-        pygame.draw.rect(self.screen, (0, 120, 176),
-                         (23, 68, self.width_mana, self.height_mana))
-        self.screen.blit(self.bar_img, (3, 3))
+    def __init__(self):
+        pass
--- menu.py
+++ menu.py
@@ -1,35 +1,3 @@
-import pygame
-
-global status
-global running
-
-
 class Menu:
-    def __init__(self, screen):
-        self._options = []
-        self._callbacks = []
-        self._current_option_index = 0
-        self.image = pygame.Surface((1280, 1080))
-        self.screen = screen
-        self.image.fill('black')
-
-    def append_option(self, option, callback):
-        self._options.append(pygame.font.SysFont('arial', 140).render(option, True, (255, 255, 255)))
-        self._callbacks.append(callback)
-
-    def switch(self, direction):
-        # тут будет звук свича
-        self._current_option_index = max(0, min(self._current_option_index + direction, len(self._options) - 1))
-
-    def select(self):
-        # тут будет звук селекта
-        self._callbacks[self._current_option_index]()
-
-    def run(self, x, y, option_y_padding):
-        self.screen.blit(self.image, (0, 0))
-        for j, option in enumerate(self._options):
-            option_rect: pygame.Rect = option.get_rect()
-            option_rect.topleft = (x, y + j * option_y_padding)
-            if j == self._current_option_index:
-                pygame.draw.rect(self.screen, (73, 74, 73), option_rect)
-            self.screen.blit(option, option_rect)
+    def __init__(self):
+        pass
