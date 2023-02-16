98848fa2f7fca106b9b946934ef86cd5a841fbee
OlyaGrekova <118425443+OlyaGrekova@users.noreply.github.com>
2023-02-16 22:09:00 +0300

    mob

--- mob.py
+++ mob.py
@@ -5,9 +5,12 @@ from random import randint
 class Mob(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.image.load('graphics\\mobs\\demon\\idle\\frame-01.png')
-        self.image = pygame.transform.scale(self.image, (100, 100))
-        self.rect = self.image.get_rect(topleft=pos)
+        self.cur_frame = 0
+        self.delay = 0
+        self.flip = False
+        self.images = [pygame.transform.scale(pygame.image.load(f'graphics\\mobs\\demon\\mob-{i + 1}.png'), (100, 100))
+                       for i in range(8)]
+        self.rect = self.images[0].get_rect(topleft=pos)
         self.vector = pygame.math.Vector2(0, 0)
         self.v = 3
         self.health = 25
@@ -25,3 +28,15 @@ class Mob(pygame.sprite.Sprite):
     def with_gravity(self):
         self.vector.y += self.gravity
         self.rect.y += self.vector.y
+        
+    def animate_mob(self, sheet, num, size, flip):
+        if self.delay == 2:
+            self.cur_frame = (self.cur_frame + 1) % num
+            self.delay = 0
+        else:
+            self.delay += 1
+        self.image = sheet[self.cur_frame]
+        self.image = pygame.transform.scale(self.image, (size[0], size[1]))
+        if flip:
+            self.image = pygame.transform.flip(self.image, True, False)
+        self.image.set_colorkey((255, 255, 255))
